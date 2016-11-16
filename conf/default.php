a3298fdc58ab8d0eb030bee8c7187da9752531f4
Andreas Gohr <gohr@cosmocode.de>
2016-11-16 15:11:01 +0100

    initial wizard generated commit

---
+++ conf/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Default settings for the structstatus plugin
+ *
+ * @author Andreas Gohr <dokuwiki@cosmocode.de>
+ */
+
+//$conf['fixme']    = 'FIXME';
---
+++ conf/metadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the structstatus plugin
+ *
+ * @author Andreas Gohr <dokuwiki@cosmocode.de>
+ */
+
+
+//$meta['fixme'] = array('string');
+
